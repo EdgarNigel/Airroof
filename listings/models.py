b5ad14de8cf90a62e9de42be34a31d1bdb807247
Philipp Laurim <philipplaurim@gmail.com>
2018-06-16 20:53:24 +0200

    Initial commit

---
+++ listings/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.db.models.signals import pre_save, post_save
+
+from .utils import unique_slug_generator
+
+# Create your models here.
+class Listing(models.Model):
+    name        = models.CharField(max_length=120)
+    location    = models.CharField(max_length=120, null=True, blank=True)
+    category    = models.CharField(max_length=120, null=True, blank=True)
+    timestamp   = models.DateTimeField(auto_now_add=True)
+    updated     = models.DateTimeField(auto_now=True)
+    slug        = models.SlugField(null=True, blank=True)
+    def __str__(self):
+        return self.name
+
+    # title needed for unique slug generator (instance.title)
+    @property
+    def title(self):
+        return self.name #ability to add obj.title
+
+def listing_pre_save_receiver(sender, instance, *args, **kwargs):
+    print('saving..')
+    print(instance.timestamp)
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+# def listing_post_save_receiver(sender, instance, *args, **kwargs):
+#     print('saved..')
+#     print(instance.timestamp)
+#     if not instance.slug:
+#         instance.slug = unique_slug_generator(instance)
+#         instance.save()
+
+pre_save.connect(listing_pre_save_receiver, sender=Listing)
+
+# post_save.connect(listing_post_save_receiver, sender=Listing)
---
+++ listings/views.py
@@ -0,0 +1,57 @@
+from django.db.models import Q
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+from django.views.generic import TemplateView, ListView, DetailView, CreateView
+
+from .forms import ListingCreateForm
+from .models import Listing
+
+# Create your views here.
+def listing_createview(request):
+    form = ListingCreateForm(request.POST or None)
+    errors = None
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect("/listings")
+    if form.errors:
+        errors = form.errors
+
+    template_name = 'listings/form.html'
+    context = {"form": form, "errors": errors}
+    return render(request, template_name, context)
+
+
+def listings_listview(request):
+    template_name = 'listings/listings_list.html'
+    queryset = Listing.objects.all()
+    context = {
+        "object_list": queryset
+    }
+    return render(request, template_name, context)
+
+class ListingListview(ListView):
+    def get_queryset(self):
+        print(self.kwargs)
+        slug = self.kwargs.get("slug")
+        if slug:
+            queryset = Listing.objects.filter(
+            Q(category__iexact=slug) |
+            Q(category__icontains=slug)
+            )
+        else:
+            queryset = Listing.objects.all()
+        return queryset
+
+class ListingDetailview(DetailView):
+    queryset = Listing.objects.all()
+
+    # def get_object(self, *args, **kwargs):
+    #     listing_id = self.kwargs.get('listing_id')
+    #     obj = get_object_or_404(Listing, id=listing_id )
+    #     return obj
+
+class ListingCreateView(CreateView):
+    form_class = ListingCreateForm
+    template_name = 'listings/form.html'
+    success_url = "/listings"
